32571fad84b4d3994aea61c7c683b516b286c47c
Rui Xu <xuruiapp2018@163.com>
2020-10-27 12:10:50 +0000

    correct the docker image name

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,12 +5,12 @@
 #docker build -t doggyclassifier:1.0 .
 
 
-dockerpath="ruixxxx/doggyclassifier"
+dockerpath="ruixxxx/doggyclassifier:1.0"
 
 # Authenticate & Tag
 echo "Docker ID and Image: $dockerpath"
 docker login &&\
-    docker image tag doggyclassifier $dockerpath
+    docker image tag doggyclassifier:1.0 $dockerpath
 
 # Push Image
 docker image push $dockerpath
