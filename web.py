ff1a1b46ed82846e01ccbd6e6099ed704698ecd4
harryshu1997 <70873553+harryshu1997@users.noreply.github.com>
2020-10-27 22:41:30 +0800

    add model to web.py

--- web.py
+++ web.py
@@ -3,6 +3,18 @@ import numpy as np
 from flask import Flask, request, render_template, flash, redirect
 from werkzeug.utils import secure_filename
 from tensorflow import keras
+from google.cloud import automl
+
+ project_id = "automl-model-293309"
+ model_id = "dogs_kaggle_20201027083024"
+ 
+
+prediction_client = automl.PredictionServiceClient()
+
+# Get the full path of the model.
+model_full_id = automl.AutoMlClient.model_path(
+    project_id, "us-central1", model_id
+)
 
 app = Flask(__name__)
 
@@ -13,12 +25,27 @@ def allowed_file(filename):
 
 # Convert inputted image to an array
 def process_image(file_path):
-    image_array = []
-    image_prep = keras.preprocessing.image.load_img(file_path, target_size = (28, 28, 3))
-    image_prep = keras.preprocessing.image.img_to_array(image_prep)
-    image_prep = image_prep/255
-    image_array.append(image_prep)
-    return np.array(image_array)
+   # Read the file.
+    with open(file_path, "rb") as content_file:
+        content = content_file.read()
+    image = automl.Image(image_bytes=content)
+    payload = automl.ExamplePayload(image=image)
+    # params is additional domain-specific parameters.
+    # score_threshold is used to filter the result
+    # https://cloud.google.com/automl/docs/reference/rpc/google.cloud.automl.v1#predictrequest
+    params = {"score_threshold": "0.8"}
+
+    request = automl.PredictRequest(
+        name=model_full_id,
+        payload=payload,
+        params=params
+    )
+    response = prediction_client.predict(request=request)
+    print("Prediction results:")
+    for result in response.payload:
+    print("Predicted class name: {}".format(result.display_name))
+    print("Predicted class score: {}".format(result.classification.score))
+    return 
 
 #@app.route("/")
 #def home():
@@ -43,14 +70,14 @@ def upload():
             )
             f.save(file_path)
             
-            #image = process_image(file_path)
-            
-            
+            image = process_image(file_path)
+             
     return
 
 @app.route("/predict")
 def predict():
-    return "predict result"
+    
+    return "Done"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9898, debug=True)
