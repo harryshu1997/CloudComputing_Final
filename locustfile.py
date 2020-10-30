3b09df240b420268f26fc583a34acc5820bba3a3
Rui Xu <xuruiapp2018@163.com>
2020-10-30 03:32:14 +0000

    modify locustfile.py and finish load test with 2 endpoints

--- locustfile.py
+++ locustfile.py
@@ -2,9 +2,9 @@
 '''
 usage:
 pip3 install locustio
-
+pip3 install locust
 locust -f locustfile.py --host=://xxx.com
-
+locust -f locustfile.py --host=http://34.66.176.215:8080/
 without web view:
     locust -f locustfile.py --host=://xxx.com --no-web -c 1000 -r 100
     -c: 1000 次点击
@@ -12,8 +12,10 @@ without web view:
 '''
 
 import sys, random
-from locust import HttpLocust, TaskSet
+#from locust import HttpLocust, TaskSet
+from locust import HttpUser, between, task
 
+'''
 def getList(locust):
     """ define a function in python whose name is getList and the argument is locust """
     locust.client.get('/')
@@ -26,11 +28,8 @@ def post_img(self):
             data={'id': self.id},
             files={'photo': image}
         )
+'''
 
-
-class MyTaskSet(TaskSet):
-    """ the class MyTaskSet inherits from the class TaskSet, defining the behavior of the user """
-    tasks = {getList: 2, post_image: 1}
     # def on_start(locust):
     #     """ on_start is called when a Locust start before any task is scheduled """
     #     response = locust.client.post("/login", data={"username":"cs144", "password": "password"})
@@ -38,8 +37,23 @@ class MyTaskSet(TaskSet):
     #         print("FAIL to start with posting data to server. Make sure that your server is running.")
     #         sys.exit();
 
-class MyLocust(HttpLocust):
-    """ the class MyLocust inherits from the class HttpLocust, representing an HTTP user """
-    task_set = MyTaskSet
-    min_wait = 1000
-    max_wait = 2000
+#class MyLocust(HttpLocust):
+class MyLocust(HttpUser):
+    wait_time = between(5,15)
+
+    @task
+    def getHome(self):
+        self.client.get('/')
+
+    @task
+    def getVersion(self):
+        self.client.get('/version')
+
+    # @task
+    # def post_img(self):
+    #     with open('img.jpg', 'rb') as image:
+    #         self.client.post(
+    #             "/static/",
+    #             data={'id': self.id},
+    #             files={'photo': image}
+    #         )
